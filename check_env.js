aea6adfc5e8f86eed302367a7089897bdb5a6d0e
林祺倫 <allen@iPhone-6.local>
2026-03-12 23:15:03 +0800

    chore: cleanup temporary test files

--- check_env.js
+++
@@ -1,8 +0,0 @@
-console.log("YT_API_KEY exists:", !!process.env.YT_API_KEY);
-console.log("HF_API_KEY exists:", !!process.env.HF_API_KEY);
-if (process.env.YT_API_KEY) {
-    console.log("YT_API_KEY prefix:", process.env.YT_API_KEY.substring(0, 5));
-}
-if (process.env.HF_API_KEY) {
-    console.log("HF_API_KEY prefix:", process.env.HF_API_KEY.substring(0, 5));
-}
